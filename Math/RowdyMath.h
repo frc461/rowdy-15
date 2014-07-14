a6665599a5e9bc946bdfcae4d5c84e5ec545b720
Kristofer Rye <kristofer.rye@gmail.com>
2014-07-14 14:53:01 -0400

    We're not in Ruby anymore.
    Semicolons ;)

--- Math/RowdyMath.h
+++ Math/RowdyMath.h
@@ -1,21 +1,21 @@
 double DoubleWeightedAverage(double a, double a_weight, double b, double b_weight)
 {
-	return (((a * a_weight) + (b * b_weight)) / (a_weight + b_weight))
+	return (((a * a_weight) + (b * b_weight)) / (a_weight + b_weight));
 }
 
 void DoubleWeightedAverage_InPlace(double *result, double a, double a_weight, double b, double b_weight)
 {
-	*result = (((a * a_weight) + (b * b_weight)) / (a_weight + b_weight))
+	*result = (((a * a_weight) + (b * b_weight)) / (a_weight + b_weight));
 }
 
 float FloatWeightedAverage(float a, float a_weight, float b, float b_weight)
 {
-	return (((a * a_weight) + (b * b_weight)) / (a_weight + b_weight))
+	return (((a * a_weight) + (b * b_weight)) / (a_weight + b_weight));
 }
 
 void FloatWeightedAverage_InPlace(float *result, float a, float a_weight, float b, float b_weight)
 {
-	*result = (((a * a_weight) + (b * b_weight)) / (a_weight + b_weight))
+	*result = (((a * a_weight) + (b * b_weight)) / (a_weight + b_weight));
 }
 
 double DoubleNullZonify(double value, double null_zone)
