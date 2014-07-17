682e8ab83eb0c10a213b4c223fc5f43c97220e2b
Sam Craig <sammidysam@gmail.com>
2014-07-16 20:10:56 -0400

    Make RowdyMath functions static

--- Rowdy15/Math/RowdyMath.h
+++ Rowdy15/Math/RowdyMath.h
@@ -4,15 +4,15 @@
 class RowdyMath
 {
  public:
-	double DoubleWeightedAverage(double a, double a_weight, double b, double b_weight);
-	void DoubleWeightedAverage_InPlace(double *result, double a, double a_weight, double b, double b_weight);
-	float FloatWeightedAverage(float a, float a_weight, float b, float b_weight);
-	void FloatWeightedAverage_InPlace(float *result, float a, float a_weight, float b, float b_weight);
+	static double DoubleWeightedAverage(double a, double a_weight, double b, double b_weight);
+	static void DoubleWeightedAverage_InPlace(double *result, double a, double a_weight, double b, double b_weight);
+	static float FloatWeightedAverage(float a, float a_weight, float b, float b_weight);
+	static void FloatWeightedAverage_InPlace(float *result, float a, float a_weight, float b, float b_weight);
 
-	double DoubleNullZonify(double value, double null_zone);
-	void DoubleNullZonify_InPlace(double *value, double null_zone);
-	float FloatNullZonify(float value, float null_zone);
-	void FloatNullZonify_InPlace(float *value, float null_zone);
+	static double DoubleNullZonify(double value, double null_zone);
+	static void DoubleNullZonify_InPlace(double *value, double null_zone);
+	static float FloatNullZonify(float value, float null_zone);
+	static void FloatNullZonify_InPlace(float *value, float null_zone);
 };
 
 #endif
